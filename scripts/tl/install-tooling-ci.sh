341f03223e423be12e43e9ccd43d9b3bd31e4fae
o-o <22546578+Apollo-o@users.noreply.github.com>
2023-08-12 15:59:48 -0700

    Update install-tooling-ci.sh

--- scripts/tl/install-tooling-ci.sh
+++ scripts/tl/install-tooling-ci.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-#Dumpsterdiver
-git clone --recursive https://github.com/securing/DumpsterDiver.git /usr/share/DumpsterDiver
-cd /usr/share/DumpsterDiver
-pip install -r requirements.txt
-chmod 757 /usr/share/DumpsterDiver
-chmod +x /usr/bin/dumpsterdiver
 
 #OnionSearch
 git clone --recursive https://github.com/megadose/OnionSearch.git /usr/share/OnionSearch
@@ -25,12 +19,6 @@ cd /usr/share/phoneinfoga
 tar xvf phoneinfoga_$(uname -s)_$(uname -m).tar.gz
 ln -s /usr/share/phoneinfoga/phoneinfoga /usr/bin/phoneinfoga
 chmod +x /usr/bin/phoneinfoga
-
-#Spiderpig
-git clone --recursive https://github.com/hatlord/Spiderpig.git /usr/share/Spiderpig
-cd /usr/share/Spiderpig
-bundle install
-chmod +x /usr/bin/spiderpig
 
 #TJ-Null OSINT Notebook
 git clone --recursive https://github.com/tjnull/TJ-OSINT-Notebook.git /etc/skel/Desktop/TJ-OSINT-Notebook
