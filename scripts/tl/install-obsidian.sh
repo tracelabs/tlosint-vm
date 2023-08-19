d9f174e04dbc4fd2f08d150333ccf0f3832d082c
Tom Hocker <tom@humandecoded.io>
2023-08-18 21:47:28 -0500

    added Obsidian to Install, TL Theme

---
+++ scripts/tl/install-obsidian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Install Obsidian app image
+
+wget -O /home/osint/Obsidian-1.3.7.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.7/Obsidian-1.3.7.AppImage 
+chmod +x /home/osint/Obsidian-1.3.7.AppImage
