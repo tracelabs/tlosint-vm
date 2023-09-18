d7caa65c0003c1cf91d87783d0470c4931c947d6
Sébastien DUFOURCQ <sebastien.dufourcq@gmail.com>
2023-09-18 19:41:51 +0200

    Update Obsidian install to latest version

--- overlays/tl-overlays/etc/skel/Desktop/install-tools.sh
+++ overlays/tl-overlays/etc/skel/Desktop/install-tools.sh
@@ -85,6 +85,8 @@ else
 fi
 
 # Install Obsidian app image
-cd ~/Desktop
-wget -O Obsidian-1.3.7.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.7/Obsidian-1.3.7.AppImage 
-chmod +x Obsidian-1.3.7.AppImage
+latest_version=$(curl -sLo /dev/null/ -w %{url_effective} https://github.com/obsidianmd/obsidian-releases/releases/latest/ | grep -oP '(?<=tag/).*')
+latest_link=$(curl -s https://github.com/obsidianmd/obsidian-releases/releases/expanded_assets/$latest_version | grep -v arm64 | grep -oP '(?<=href="/).*AppImage')
+wget -O obsidian https://github.com/$latest_link
+chmod +x obsidian
+sudo mv obsidian /usr/bin/
