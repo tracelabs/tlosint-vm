8211f5555a66c616e7f33a37d7fa75fec6a770d3
Tom Hocker <tom@humandecoded.io>
2023-09-30 17:22:04 -0400

    obsidian launcher on desktop

--- scripts/tl/install-obsidian.sh
+++ scripts/tl/install-obsidian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Install Obsidian app image
 
-wget -O /home/osint/Obsidian-1.3.7.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.7/Obsidian-1.3.7.AppImage 
-chmod +x /home/osint/Obsidian-1.3.7.AppImage
+wget -O /home/osint/Obsidian.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.14/Obsidian-1.4.14.AppImage 
+chmod +x /home/osint/Obsidian.AppImage
+chmod +x /home/osint/Desktop/obsidian.desktop
