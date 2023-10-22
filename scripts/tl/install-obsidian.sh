659999b6cd604195b30aecd375c0bf21e683c4b3
Sébastien DUFOURCQ <sebastien.dufourcq@gmail.com>
2023-10-22 10:26:51 +0200

    Update install-obsidian.sh
    Get the latest Obsidian AppImage version

--- scripts/tl/install-obsidian.sh
+++ scripts/tl/install-obsidian.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Install Obsidian app image
+# Install Obsidian latest AppImage
 
-wget -O /home/osint/Obsidian.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.14/Obsidian-1.4.14.AppImage 
+latest_version=$(curl -sLo /dev/null/ -w %{url_effective} https://github.com/obsidianmd/obsidian-releases/releases/latest/ | grep -oP '(?<=tag/).*')
+latest_link=$(curl -s https://github.com/obsidianmd/obsidian-releases/releases/expanded_assets/$latest_version | grep -v arm64 | grep -oP '(?<=href="/).*AppImage')
+wget -O /home/osint/Obsidian.AppImage https://github.com/$latest_link
 chmod +x /home/osint/Obsidian.AppImage
 chmod +x /home/osint/Desktop/obsidian.desktop
